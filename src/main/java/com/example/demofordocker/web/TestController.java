e3bbdf9e0ed58fd98974d037eba82fcc21651462
Popov85 <andrey.popov85@gmail.com>
2020-02-28 13:16:18 +0200

    logging added

--- src/main/java/com/example/demofordocker/web/TestController.java
+++ src/main/java/com/example/demofordocker/web/TestController.java
@@ -2,20 +2,24 @@ package com.example.demofordocker.web;
 
 import com.example.demofordocker.service.TestService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class TestController {
 
     private final TestService testService;
 
-    @GetMapping(value="/test/data", params = "name")
-    public ResponseEntity<String> getData(@RequestParam String name) {
+    @GetMapping(value="/test/data")
+    public ResponseEntity<String> getData(@RequestParam(required = false) String name) {
+        log.debug("Request arrived with name = {}", name!=null ? name : "Name is not present");
+        if (name==null) name = "Name is not present";
         return ResponseEntity.ok(testService.getData(name));
     }
 }
